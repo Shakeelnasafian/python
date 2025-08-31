e7cdc26d3f2ca8ae1f9d7fdbff6f4e7fa158e7a8
shakeel <shakeelnasafian@gmail.com>
2025-08-31 14:15:38 +0400

    progress bar added in the transcript

--- exercise/tax.py
+++ exercise/tax.py
@@ -1,3 +1,9 @@
+import torch
+print(torch.__version__)
+print(torch.version.cuda)
+print(torch.backends.cudnn.version())
+print(torch.cuda.is_available())
+
 amount = 100
 tax = 0.06
 total = amount + amount * tax
@@ -6,3 +12,4 @@ print(total)
 hello = "hello"
 name = input("Enter your name: ")
 print(hello + " " + name)
+
